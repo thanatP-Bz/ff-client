eddea87b8bdffba425023ce8d082f8503659b930
Thanat <bztzn512@gmail.com>
2023-03-01 13:03:36 -0500

    .

--- src/hooks/useLogin.jsx
+++ src/hooks/useLogin.jsx
@@ -21,7 +21,7 @@ export const useLogin = () => {
     } catch (error) {
       dispatch({
         type: LOGIN_ERROR,
-        payload: { message: error.response.data.msg },
+        payload: { message: error.response.data },
       });
     }
     clearAlert();
